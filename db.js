7cd96c76d871baaa88343a69ec55e4f5ee31bac8
Pedro lara <plara@idt-lt-899393.indotel.gob.do>
2025-03-20 13:40:46 -0400

    cambios

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ export const pool = mysql.createPool({
   user: process.env.DB_USER || 'root', 
   password: process.env.DB_PASSWORD || 'root', 
   database: process.env.DB_NAME || 'tutordb',
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
