12c90017e74f462df8c7dc7f7bca8f7e0e4cc419
Pedro lara <plara@idt-lt-899393.indotel.gob.do>
2025-04-09 14:09:18 -0400

    cambios

--- src/routes/tutorRoutes.js
+++ src/routes/tutorRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
 import  {getTutorDetails, getAllUsers, getAllTutors, getTutorById}  from '../controllers/tutorController.js';
+import StudentsController from '../controllers/StudentsController.js';
 
 const router = express.Router();
 
@@ -19,6 +20,13 @@ router.get('/tutors', authMiddleware, getAllTutors);
 
 
 router.get('/tutors/:tutorId', authMiddleware, getTutorById);
+
+
+router.get('/students', StudentsController.getAllStudents);
+
+router.get('/:studentId', StudentsController.getStudentById);
+
+router.get('/filtered', StudentsController.getStudentsWithFilters);
 
 
 export default router;
