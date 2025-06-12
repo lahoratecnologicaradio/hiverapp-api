99f717c7c40abb42bc7c3371e5d08a428afc147a
Pedro lara <plara@idt-lt-899393.indotel.gob.do>
2025-06-12 16:28:53 -0400

    cambios

--- src/controllers/UsersVAController.js
+++ src/controllers/UsersVAController.js
@@ -160,6 +160,7 @@ saveFormData: async (req, res) => {
       res.json(rows);
     } catch (error) {
       console.error('Error obteniendo datos:', error);
+      console.error('Error obteniendo datos:', error);
       res.status(500).json({ message: 'Error interno del servidor.' });
     }
   },
